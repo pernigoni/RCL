7bc74bc66cfe253447290e830e6335bb423edd73
Lorenzo Pernigoni <l.pernigoni@outlook.it>
2024-04-20 12:00:12 +0200

    Add files via upload

---
+++ Esercizio-Serra/GreenHouseScheduler.java
@@ -0,0 +1,103 @@
+import java.time.LocalTime;
+import java.util.concurrent.*;
+
+public class GreenHouseScheduler
+{
+	private class LightOn implements Runnable
+	{
+		public void run()
+		{ System.out.println("Turning on lights"); }
+	}
+
+	private class LightOff implements Runnable
+	{
+		public void run()
+		{ System.out.println("Turning off lights"); }
+	}
+
+	private class WaterOn implements Runnable
+	{
+		public void run()
+		{ System.out.println("Turning water on"); }
+	}
+
+	private class WaterOff implements Runnable
+	{
+		public void run()
+		{ System.out.println("Turning water off"); }
+	}
+
+	private class ThermostatNight implements Runnable
+	{
+		public void run()
+		{ System.out.println("Thermostat to night setting"); }
+	}
+
+	private class ThermostatDay implements Runnable
+	{
+		public void run()
+		{ System.out.println("Thermostat to day setting"); }
+	}
+
+	private class Bell implements Runnable
+	{
+		public void run()
+		{ System.out.println("Bing!"); }
+	}
+
+	public class Terminate implements Runnable
+	{
+		ScheduledThreadPoolExecutor scheduler;
+
+		public Terminate(ScheduledThreadPoolExecutor scheduler)
+		{
+			this.scheduler = scheduler;
+		}
+
+		public void run()
+		{
+			System.out.println("Terminating");
+			scheduler.shutdownNow();
+		}
+	}
+
+	public static void schedule(ScheduledThreadPoolExecutor scheduler, Runnable event, long delay, TimeUnit tu)
+	{
+		scheduler.schedule(event, delay, tu);
+	}
+
+	public static void repeat(ScheduledThreadPoolExecutor scheduler, Runnable event, long initialDelay, long period, TimeUnit tu)
+	{
+		scheduler.scheduleAtFixedRate(event, initialDelay, period, tu);
+	}
+
+	public static void main(String[] args)
+	{
+		GreenHouseScheduler gh = new GreenHouseScheduler();
+		ScheduledThreadPoolExecutor scheduler = new ScheduledThreadPoolExecutor(10);
+
+		schedule(scheduler, gh.new Terminate(scheduler), 10000, TimeUnit.MILLISECONDS);
+		repeat(scheduler, gh.new Bell(), 0, 1000, TimeUnit.MILLISECONDS);
+		repeat(scheduler, gh.new LightOn(), 0, 200, TimeUnit.MILLISECONDS);
+		repeat(scheduler, gh.new LightOff(), 100, 200, TimeUnit.MILLISECONDS);
+		repeat(scheduler, gh.new WaterOn(), 0, 600, TimeUnit.MILLISECONDS);
+		repeat(scheduler, gh.new WaterOff(), 300, 600, TimeUnit.MILLISECONDS);
+
+		LocalTime now = LocalTime.now();
+		LocalTime morning = LocalTime.of(7, 0, 0);
+		LocalTime night= LocalTime.of(19, 0, 0);
+		Boolean light = now.isAfter(morning) && now.isBefore(night);
+		int interval = 86400;
+		if(light)
+		{
+			int seconds = night.toSecondOfDay() - now.toSecondOfDay();
+			repeat(scheduler, gh.new ThermostatDay(), 0, interval, TimeUnit.SECONDS);
+			repeat(scheduler, gh.new ThermostatNight(), seconds, interval, TimeUnit.SECONDS);}
+		else
+		{
+			int seconds = morning.toSecondOfDay() - now.toSecondOfDay();
+			repeat(scheduler, gh.new ThermostatNight(), 0, interval, TimeUnit.SECONDS);
+			repeat(scheduler, gh.new ThermostatDay(), seconds, interval, TimeUnit.SECONDS);
+		}
+	}
+}
---
+++ Esercizio-Serra/README.md
@@ -0,0 +1,13 @@
+# SIMULAZIONE DI UNA SERRA
+
+Scrivere un programma che simuli la gestione di una serra, i processi che vengono attivati periodicamente
+nella serra sono i seguenti.
+* Periodicamente vengono aperti e poi chiusi dei distributori di acqua per annaffiare le piante.
+* Periodicamente si accendono e poi si spengono dei led per fornire la luce che favorisce la fotosintesi.
+* Il termostato deve essere impostato ad un certo valore, durante le ore di luce 
+e ad un altro valore durante la notte (si supponga che le ore di luce inizino alle 
+9:00 e terminino alle 19:00).
+* Periodicamente una sorgente sonora scandisce il tempo.
+
+Il programma deve simulare i processi precedenti emettendo delle stampe che indicano l'occorrenza del
+corrispondente evento.
